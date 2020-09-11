a996776508c59db93f1020fa057ff9ff54a5a0de
andrea.tricoli <andrea.tricoli@docomodigital.com>
2020-09-11 11:22:00 +0200

    fix ios

--- js/detect-ios.js
+++ js/detect-ios.js
@@ -1,6 +1,11 @@
 var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 if (isIOS) {
-  console.log('This is a IOS device');
+	console.log('This is a IOS device');
+	var list = document.querySelectorAll(".special-section");
+	for(var i=0; i<list.length; i++){
+		list[i].classList.remove('special-section');
+		list[i].classList.add('special-section-ios-version');
+	}
 } else {
-  console.log('This is Not a IOS device');
+	console.log('This is Not a IOS device');
 }
